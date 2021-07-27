c98c8bfc8ea6cf511ca9c11efe070e4c5089e0d8
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2021-07-27 00:22:47 -0400

    server completed

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -60,6 +60,16 @@ const resolvers = {
 
                 )
             }
+            throw new AuthenticationError('You need to be logged in!');
+        },
+
+        removeBook: async (parent,{ bookId },context ) =>{
+            if (context.user) {
+                return User.findOneAndUpdate({ _id: context.user._id },
+                    { $pull: { savedBooks: { bookId } } },
+                    { new: true });
+            }
+            throw new AuthenticationError('You need to be logged in!');
         }
 
     }
